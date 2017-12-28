1c487ee4cf6f12b706a065aceb42e1a870d730f7
zhangjianchong <zhangjianchong@rmitec.com>
2017-12-29 00:04:32 +0800

    实现启动

--- src/main/java/com/zhangjc/HelloController.java
+++ src/main/java/com/zhangjc/HelloController.java
@@ -2,14 +2,16 @@ package com.zhangjc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by YJ on 2017/12/28.
  */
-@Controller
+@RestController
 public class HelloController {
     @RequestMapping("/hello")
     public String sayHello(){
+        System.out.println("终于解决了");
         return "hello world";
     }
 }
