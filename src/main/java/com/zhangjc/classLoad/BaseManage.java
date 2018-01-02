8777b3d54bd2e884386c7185b04ce998e7c7bce7
zhangjianchong <1204880901@qq.com>
2018-01-02 16:37:22 +0800

    热加载测试

---
+++ src/main/java/com/zhangjc/classLoad/BaseManage.java
@@ -0,0 +1,8 @@
+package com.zhangjc.classLoad;
+
+/**
+ * Created by YJ on 2018/1/2.
+ */
+public interface BaseManage {
+    public void logic();
+}
---
+++ src/main/java/com/zhangjc/classLoad/BaseManageImpl.java
@@ -0,0 +1,11 @@
+package com.zhangjc.classLoad;
+
+/**
+ * Created by YJ on 2018/1/2.
+ */
+public class BaseManageImpl implements BaseManage {
+    @Override
+    public void logic() {
+
+    }
+}
